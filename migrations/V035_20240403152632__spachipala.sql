26a552a788b7c14e44a311123f7aea1e51f9d69a
Sandeep Pachipala <SPachipala@surveymonkey.com>
2024-04-04 11:55:18 -0700

    new changes

---
+++ migrations/V035_20240403152632__spachipala.sql
@@ -0,0 +1,18 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[Training]'
+GO
+CREATE TABLE [dbo].[Training]
+(
+[TrainingId] [int] NOT NULL CONSTRAINT [DF__Training__TrainingId] DEFAULT (NEXT VALUE FOR [SequenceCounter]),
+[TrainingName] [nvarchar] (50) NOT NULL,
+[TrainingDate] [date] NOT NULL
+)
+GO
+PRINT N'Creating primary key [PK__Training__E8D71D829049BE32] on [dbo].[Training]'
+GO
+ALTER TABLE [dbo].[Training] ADD CONSTRAINT [PK__Training__E8D71D829049BE32] PRIMARY KEY CLUSTERED ([TrainingId]) WITH (ONLINE = ON)
+GO
+
