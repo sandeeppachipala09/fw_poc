ebd395d861733c6bf281f52007dd1b63b335dd89
sandeeppachipala <163978888+sandeeppachipala@users.noreply.github.com>
2024-04-04 14:43:15 -0700

    Update dbo.test2.sql

--- schema-model/Tables/dbo.test2.sql
+++ schema-model/Tables/dbo.test2.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[test2]
 [id] [int] NULL,
 [Fname] [varchar] (20) NULL,
 [Lname] [varchar] (30) NULL,
-[address] [varchar] (200) NULL
+[address] [varchar] (200) NULL,
+[phonenumber] bigint null
 )
 GO
